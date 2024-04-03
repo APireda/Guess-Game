bedf2115d78df1ac7b2530b3e89f594ca6d136ae
Ayrton Pireda <piredaayrton@gmail.com>
2024-04-03 16:58:01 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,54 @@
+const initialScreen = document.querySelector('.initial-screen')
+const guessText = document.querySelector('.initial-screen p')
+const finishedScreen = document.querySelector('.finished-screen')
+const finishedText = finishedScreen.querySelector('h2')
+let randomNumber = Math.round(Math.random() * 10)
+let attempts = 1
+
+const buttonTry = document.querySelector('#button-try')
+const buttonReset = document.querySelector('#button-reset')
+
+const toggleScreen = () => {
+  initialScreen.classList.toggle('hide')
+  finishedScreen.classList.toggle('hide')
+}
+
+const handleClick = (event) => {
+  event.preventDefault()
+
+  const numberInput = document.querySelector('#numberInput')
+  const rightAnswer = numberInput.value == randomNumber
+
+  if(numberInput.value < 0 || numberInput.value > 10) {
+    alert('O número deve ser entre 0 e 10')
+    return
+  }
+
+  if(rightAnswer) {
+    toggleScreen()
+
+    attempts > 1 ? finishedText.innerText = `Acertou em ${attempts} tentativas!` : finishedText.innerText = `Acertou em ${attempts} tentativa!`
+
+    return
+  }
+
+  guessText.innerText = `${attempts}° tentativa, tente novamente!`
+
+  attempts++
+  numberInput.value = ''
+}
+
+const handleResetClick = (event) => {
+  event.preventDefault()
+
+  toggleScreen()
+
+  attempts = 1
+
+  guessText.textContent = 'Adivinhe o número entre 0 e 10'
+  numberInput.value = ''
+  randomNumber = Math.round(Math.random() * 10)
+}
+
+ buttonTry.addEventListener('click', handleClick)
+ buttonReset.addEventListener('click', handleResetClick)
